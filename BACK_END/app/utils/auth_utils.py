285a6d1c740164deebfc77925bdb66e9e170bacf
Nithy1102 <anantham1102@gmail.com>
2025-07-25 23:18:44 +0530

    HUH

---
+++ BACK_END/app/utils/auth_utils.py
@@ -0,0 +1,42 @@
+import jwt
+import datetime
+from config import config
+from flask import request,jsonify
+from functools import wraps
+
+
+def create_token(name,role):
+ try:
+    payload={
+        "user":name,
+        "user_role":role,
+        "exp":datetime.datetime.utcnow()+datetime.timedelta(hours=12)}
+
+    token=jwt.encode(payload,config.key,algorithm="HS256")
+    return token
+ except Exception as e:
+   return None
+
+def token_req(f):
+  @wraps(f)
+  def decorated(*arg,**kwarg):
+    token=None
+    if "x-access-token" in request.headers():
+      token=request.headers["x-access-token"]
+    if not token:
+      return jsonify({"mes":"missing token"}),401
+    
+    try:
+      data=jwt.decode(token,config.key,algorithm="HS256")
+
+      cur_user={
+        "user_id":data["user_id"],
+        "role":data["role"]
+      }
+    except jwt.ExpiredSignatureError:
+      return jsonify({"mes":"TOKEN HAS EXPIRED"})
+    except jwt.InvalidTokenError:
+      return jsonify({"mes":"INVALID TOKEN"}),401
+   
+    return f(cur_user,*arg,**kwarg)
+  return decorated
